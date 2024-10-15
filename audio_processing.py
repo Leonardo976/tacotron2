bfb0bf1a017dd32fee4c5bd26fc04e5a90725f6f
Leonardo976 <leonardo.munoz.f@usach.cl>
2024-10-15 16:29:38 -0300

    Ajustes menores en el procesamiento de audio

--- audio_processing.py
+++ audio_processing.py
@@ -3,40 +3,10 @@ import numpy as np
 from scipy.signal import get_window
 import librosa.util as librosa_util
 
-
-def window_sumsquare(window, n_frames, hop_length=200, win_length=800,
-                     n_fft=800, dtype=np.float32, norm=None):
+def window_sumsquare(window, n_frames, hop_length=256, win_length=1024,
+                     n_fft=1024, dtype=np.float32, norm=None):
     """
-    # from librosa 0.6
     Compute the sum-square envelope of a window function at a given hop length.
-
-    This is used to estimate modulation effects induced by windowing
-    observations in short-time fourier transforms.
-
-    Parameters
-    ----------
-    window : string, tuple, number, callable, or list-like
-        Window specification, as in `get_window`
-
-    n_frames : int > 0
-        The number of analysis frames
-
-    hop_length : int > 0
-        The number of samples to advance between frames
-
-    win_length : [optional]
-        The length of the window function.  By default, this matches `n_fft`.
-
-    n_fft : int > 0
-        The length of each analysis frame.
-
-    dtype : np.dtype
-        The data type of the output
-
-    Returns
-    -------
-    wss : np.ndarray, shape=`(n_fft + hop_length * (n_frames - 1))`
-        The sum-squared envelope of the window function
     """
     if win_length is None:
         win_length = n_fft
@@ -55,7 +25,6 @@ def window_sumsquare(window, n_frames, hop_length=200, win_length=800,
         x[sample:min(n, sample + n_fft)] += win_sq[:max(0, min(n_fft, n - sample))]
     return x
 
-
 def griffin_lim(magnitudes, stft_fn, n_iters=30):
     """
     PARAMS
@@ -63,7 +32,6 @@ def griffin_lim(magnitudes, stft_fn, n_iters=30):
     magnitudes: spectrogram magnitudes
     stft_fn: STFT class with transform (STFT) and inverse (ISTFT) methods
     """
-
     angles = np.angle(np.exp(2j * np.pi * np.random.rand(*magnitudes.size())))
     angles = angles.astype(np.float32)
     angles = torch.autograd.Variable(torch.from_numpy(angles))
@@ -74,7 +42,6 @@ def griffin_lim(magnitudes, stft_fn, n_iters=30):
         signal = stft_fn.inverse(magnitudes, angles).squeeze(1)
     return signal
 
-
 def dynamic_range_compression(x, C=1, clip_val=1e-5):
     """
     PARAMS
@@ -82,7 +49,6 @@ def dynamic_range_compression(x, C=1, clip_val=1e-5):
     C: compression factor
     """
     return torch.log(torch.clamp(x, min=clip_val) * C)
-
 
 def dynamic_range_decompression(x, C=1):
     """
